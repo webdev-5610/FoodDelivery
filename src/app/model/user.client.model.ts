d06962c3374a347a4537ba9698310d9a4d7e04b8
zhilin <fang.zhil@husky.neu.edu>
2019-04-07 23:33:33 -0700

    2

--- src/app/model/user.client.model.ts
+++ src/app/model/user.client.model.ts
@@ -6,7 +6,7 @@ export class User {
   lastName: String;
   email: String;
 
-
+  name, password, email, addrress, phone, credit card info, billing info, past orders
   constructor( username: String, password: String, firstName: String, lastName: String, email: String) {
     this.username = username;
     this.password = password;
