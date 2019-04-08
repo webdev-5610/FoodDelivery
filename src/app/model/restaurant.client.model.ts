8def49a7fa4d7828ab2f8d21314a8177ea281ae9
zhilin <fang.zhil@husky.neu.edu>
2019-04-08 01:14:04 -0700

    3

--- src/app/model/restaurant.client.model.ts
+++ src/app/model/restaurant.client.model.ts
@@ -1,12 +1,40 @@
-export class Restaurant {
-  _id: String;
-  name: String;
-  websiteId: String;
-  title: String;
+import {Order} from "./order.client.model";
 
-  constructor(name: String, websiteId: String, description: String) {
-    this.name = name;
-    this.websiteId = websiteId;
-    this.title = description;
-  }
+class Menu {
+    _id: String;
+    dish_name: String;
+    price: Number;
+    description: String;
+
+    constructor(name: String, price: Number, description: String) {
+        this.description = description;
+        this.dish_name = name;
+        this.price = price;
+    }
+
+}
+
+
+export class Restaurant {
+    _id: String;
+    name: String;
+    password: String;
+    email: String;
+    address: String;
+    phone: String;
+    type: String;
+    oders: Order[];
+    menus: Menu[];
+
+
+    constructor(name: String, password: String, email: String, address: String, phone: String, type: String, orders: Order[], menus: Menu[]) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.type = type;
+        this.oders = orders;
+        this.menus = menus;
+    }
 }
