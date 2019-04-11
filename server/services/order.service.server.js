82633fdd5d0840d13b0191d052e03e8051e8c965
zhilin <fang.zhil@husky.neu.edu>
2019-04-11 07:59:07 -0700

    order service

--- server/services/order.service.server.js
+++ server/services/order.service.server.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   // fake data
-  // var pages=[
+  // var orders=[
   //   { "_id": "321", "name": "Post 1", "websiteId": "456", "title": "Lorem" },
   //   { "_id": "432", "name": "Post 2", "websiteId": "456", "title": "Lorem" },
   //   { "_id": "543", "name": "Post 3", "websiteId": "456", "title": "Lorem" }
@@ -10,18 +10,20 @@ module.exports = function(app) {
   const orderModel = require('../model/order/order.model.server');
   // api list
 
-    app.post('/api/restaurant/:websiteId/order', createOrder);
-    app.get('/api/restaurant/:websiteId/order', findOrdersByWebsite);
-    app.get('/api/restaurant/:websiteId/order/:pageId', findOrderById);
-    app.put('/api/restaurant/:websiteId/order/:pageId', updateOrder);
-    app.delete('/api/restaurant/:websiteId/order/:pageId', deleteOrder);
+    app.post('/api/:userId/order', createOrder);
+    app.get('/api/:userId/order', findOrdersByUser);
+    app.get('/api/:userId/order/:orderId', findOrderById);
+    app.put('/api/:userId/order/:orderId', updateOrder);// also check status if finished or not.
+    app.delete('/api/:userId/order/:orderId', deleteOrder);
+    app.put('/api/:restaurantId/order',finishOrder);
+    app.get('/api/:restaurantId/order',findAllOrdersByRestaurant);
 
   // function list
   function createOrder(req, res) {
-    var page = req.body;
+    var order = req.body;
     var websiteId = req.params['websiteId'];
-    page._website = websiteId;
-    delete page._id;
+    order._website = websiteId;
+    delete order._id;
     orderModel.createOrder(websiteId, order)
         .then(function (order) {
               res.status(200).send(order);
@@ -35,7 +37,7 @@ module.exports = function(app) {
             });
   }
 
-  function findOrdersByWebsite(req, res) {
+  function findOrdersByUser(req, res) {
     var websiteId = req.params['websiteId'];
     orderModel.findAllOrdersForWebsite(websiteId)
         .then(function (orders) {
