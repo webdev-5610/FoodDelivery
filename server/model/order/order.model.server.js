7fed00c53fcefe1b25dceac3da6705f9a5cb9482
Lainka617 <luo.shiq@husky.neu.edu>
2019-04-18 00:53:19 -0700

    fixes

--- server/model/order/order.model.server.js
+++ server/model/order/order.model.server.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var userModel = require('server/model/user/user.model.server');
+var userModel = require('../user/user.model.server');
 var orderSchema = require('./order.schema.server');
 var orderModel = mongoose.model("order",orderSchema);
 var restaurantModel = require("../restaurant/restaurant.model.server");
@@ -9,7 +9,7 @@ orderModel.findAllOrdersByDeliver = findAllOrdersByDeliver;
 orderModel.findOrderById = findOrderById;
 orderModel.updateOrder = updateOrder;// also check status if finished or not.
 orderModel.deleteOrder = deleteOrder;
-orderMedel.createOrder = createOrder;
+orderModel.createOrder = createOrder;
 orderModel.finishOrder = finishOrder;
 orderModel.postOrder = postOrder;
 orderModel.acceptOrder=acceptOrder;
