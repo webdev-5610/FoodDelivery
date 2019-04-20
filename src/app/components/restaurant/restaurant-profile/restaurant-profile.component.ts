41b4b7e91e59bf7a64f3edf0a319080aaf9d3311
zhilin <fang.zhil@husky.neu.edu>
2019-04-19 23:58:50 -0700

    profile

--- src/app/components/restaurant/restaurant-profile/restaurant-profile.component.ts
+++ src/app/components/restaurant/restaurant-profile/restaurant-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {SharedService} from '../../../services/shared.service';
-import {RestaurantService} from '../../../services/restaurant.service.client'
+import {RestaurantService} from '../../../services/restaurant.service.client';
 import {Restaurant} from "../../../model/restaurant.client.model";
 
 @Component({
--- src/app/components/user/user-profile/user-profile.component.ts
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../../services/user.service.client';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SharedService} from '../../../services/shared.service';
+import {User} from '../../../model/user.client.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  errorMsg = 'Invalid email address !';
+
+  constructor(private userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private sharedService: SharedService) {
+    this.user = new User('', '', '', '', '', '', '', '');
+  }
+
+  restaurantId: String;
+  user: User;
+  username: String;
+  updateFlag: boolean;
+  updateMsg: String;
+  userErrorFlag: boolean;
+  userErrorMsg: String;
+
+  updateUser(user: User) {
+    this.updateFlag = false;
+    this.userErrorFlag = false;
+    if (this.username !== this.user.username) {
+      this.userService.findUserByUsername(this.username).subscribe(
+          (user: User) => {
+            if (typeof user._id !== 'undefined') {
+              this.userErrorFlag = true;
+            } else {
+              this.user.username = this.username;
+              this.updateProfile();
+            }
+          },
+          (error: any) => console.log(error)
+      );
+    } else {
+      this.updateProfile();
+    }
+  }
+
+  updateProfile() {
+    this.restaurantService.updateRestaurant(this.restaurant._id, this.restaurant).subscribe(
+        (restaurant: Restaurant) => {
+          console.log(restaurant);
+          this.restaurant = restaurant;
+          this.updateFlag = true;
+        },
+        (error: any) => {
+          console.log(error);
+        }
+    );
+  }
 
   ngOnInit() {
+    this.restaurant = this.sharedService.user;
+    this.restaurantId = this.sharedService.user._id;
+    this.route.params.subscribe(params => {
+      this.updateFlag = false;
+      this.userErrorFlag = false;
+      this.updateMsg = 'Profile updated!';
+      this.userErrorMsg = 'The username already exists! Please use a different name.';
+      this.restaurantService.findRestaurantById(this.restaurantId).subscribe(
+          (restaurant: Restaurant) => {
+            console.log((restaurant));
+            this.restaurant = restaurant;
+            this.username = this.restaurant.username;
+          },
+          (error: any) => console.log(error)
+      );
+    });
   }
 
 }
