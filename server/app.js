e6527da0cb24dfa154d80b3867fd28865b9245c4
Lainka617 <luo.shiq@husky.neu.edu>
2019-04-22 23:01:28 -0700

    fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 module.exports = function (app){
   require('./services/user.service.server')(app);
-  require("./services/restaurant.service.server")(app);
   require("./services/order.service.server")(app);
   require('./services/deliver.service.server')(app);
   require("./services/menu.service.server")(app);
