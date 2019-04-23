516e7cb6284d350fb1e3ff3eb195bd14d0b73f88
Lainka617 <luo.shiq@husky.neu.edu>
2019-04-22 17:20:30 -0700

    updates2

--- src/app/services/deliver.service.client.ts
+++ src/app/services/deliver.service.client.ts
@@ -1,45 +1,13 @@
 import {Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Order } from '../model/order.client.model';
-import { Observable, ObjectUnsubscribedError } from 'rxjs';
 
 @Injectable()
 export class DeliverService {
 
     constructor(private http: HttpClient) {}
 
-    api = {
-        'acceptOrder' : this.acceptOrder,
-        'completeOrder' : this.completeOrder,
-        'cancelOrder' : this.cancelOrder
-    };
-
     public mapAPIKey = 'AIzaSyBtrCeFbuL6cSgjC2UyJsaJuJoXKXAmQQM';
-
-    acceptOrder(deliverId: string, orderId: string, status: string) {
-        // return this.http.put( environment.baseUrl + '/api/deliver/' + deliverId + '/order/' + orderId + '/accept', {});
-        return new Observable((observer) => {
-            observer.next({content: true});
-            return {unsubscribe() {}};
-        });
-    }
-
-    completeOrder(deliverId: string, orderId: string, status: string) {
-        // return this.http.put( environment.baseUrl + '/api/deliver/' + deliverId + '/order/' + orderId + '/complete', {});
-        return new Observable((observer) => {
-            observer.next({content: true});
-            return {unsubscribe() {}};
-        });
-    }
-
-    cancelOrder(deliverId: string, orderId: string, status: string) {
-        // return this.http.put( environment.baseUrl + '/api/deliver/' + deliverId + '/order/' + orderId + '/cancel', {});
-        return new Observable((observer) => {
-            observer.next({content: true});
-            return {unsubscribe() {}};
-        });
-    }
 
     calculate(lat: String, lng: String, destination: String) {
         return this.http.post(environment.baseUrl + '/api/deliver/getdistance', {
